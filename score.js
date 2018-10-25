9d71c0fb811d0e4f737256046d5de05ca4faa1c7
Unknown <AlixAkhribi@yahoo.com>
2018-10-24 18:07:32 -0700

    onScoreUpdate to collect data and push it to output
    onScoreUpdate function will take the data consisting of dropPosition, bounciness, size, bucketLabel, and push it into output. 
    
    output will contain all the data collected from each ball drop

--- score.js
+++ score.js
@@ -1,5 +1,11 @@
+// output = [dropPosition, bounciness, size, bucketLabel]
+const output = []
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  output.push([dropPosition, bounciness, size, bucketLabel])
+
+  
 }
 
 function runAnalysis() {
